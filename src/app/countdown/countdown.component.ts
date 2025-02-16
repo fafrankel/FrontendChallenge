630729d3a7c86f4efa3281fb037f597bf01067c8
fafrankel <fa.frankel@gmail.com>
2025-02-16 18:55:54 +0100

    resize font & persist data in local storage

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'countdown',
@@ -7,21 +7,66 @@ import { Component } from '@angular/core'
   styleUrl: './countdown.component.scss',
 })
 export class CountdownComponent {
-  eventName = ''
-  eventDate: Date | null = null
+  eventTitle = ''
+  eventDate: string = ''
+  eventTitleId = 'event-title'
+  dateTitleId = 'date-title'
+  textContainerId = 'title-container'
+
+  ngOnInit() {
+    this.eventTitle = localStorage.getItem('eventTitle') || ''
+    this.eventDate = localStorage.getItem('eventDate') || ''
+
+    console.log('Init', this.eventTitle, this.eventDate)
+  }
+
+  ngAfterViewInit() {
+    this.adjustFontSizes()
+    window.addEventListener('resize', () => this.adjustFontSizes())
+  }
+
+  adjustFontSizes() {
+    this.adjustFontSize(this.eventTitleId, this.textContainerId)
+    this.adjustFontSize(this.dateTitleId, this.textContainerId)
+  }
+
+  adjustFontSize(id: string, containerId: string) {
+    const element = document.getElementById(id)
+    const containerElement = document.getElementById(containerId)
+    if (!element || !containerElement) return
+
+    let fontSize = 200
+    element.style.fontSize = `${fontSize}px`
+    while (element.clientWidth > containerElement.clientWidth && fontSize > 10) {
+      fontSize -= 1
+      element.style.fontSize = `${fontSize}px`
+    }
+  }
 
   onEventNameChange(e: Event) {
-    this.eventName = (e.target as HTMLInputElement).value
+    const value = (e.target as HTMLInputElement).value
+    localStorage.setItem('eventTitle', value)
+    this.eventTitle = value
+    setTimeout(() => this.adjustFontSize(this.eventTitleId, this.textContainerId), 30)
   }
 
   onDateChange(e: Event) {
-    this.eventDate = new Date((e.target as HTMLInputElement).value)
+    const value = (e.target as HTMLInputElement).value
+    localStorage.setItem('eventDate', value)
+    this.eventDate = value
+    setTimeout(() => this.adjustFontSize(this.dateTitleId, this.textContainerId), 30)
+  }
+
+  formattedEventTitleString() {
+    if (!this.eventTitle) return ''
+    return `Time to ${this.eventTitle}`
   }
 
   formattedDateString() {
     if (!this.eventDate) return ''
     const startDate = new Date()
-    let diffInSeconds = Math.floor((this.eventDate.getTime() - startDate.getTime()) / 1000)
+    const eventDate = new Date(this.eventDate)
+    let diffInSeconds = Math.floor((eventDate.getTime() - startDate.getTime()) / 1000)
 
     const days = Math.floor(diffInSeconds / (24 * 3600))
     diffInSeconds %= 24 * 3600
